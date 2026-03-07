6c97c2c1ed0691d782f1e3d9e8692bb1024606ec
fr33root5 <goaangel@yahoo.de>
2026-03-07 07:50:29 +0100

    Add Spicetify runtime colors support
    - Add spicetify-colors.json template
    - Add spicetify.toml config
    - Register spicetify in templateRegistry
    - Colors will be written to ~/.config/spicetify/colors.json
    - Enables instant color updates without rebuild

--- core/internal/matugen/matugen.go
+++ core/internal/matugen/matugen.go
@@ -58,6 +58,7 @@ var templateRegistry = []TemplateDef{
 	{ID: "qt6ct", Commands: []string{"qt6ct"}, ConfigFile: "qt6ct.toml"},
 	{ID: "firefox", Commands: []string{"firefox"}, ConfigFile: "firefox.toml"},
 	{ID: "pywalfox", Commands: []string{"pywalfox"}, ConfigFile: "pywalfox.toml"},
+	{ID: "spicetify", Commands: []string{"spotify", "spicetify"}, ConfigFile: "spicetify.toml"},
 	{ID: "zenbrowser", Commands: []string{"zen", "zen-browser", "zen-beta", "zen-twilight"}, Flatpaks: []string{"app.zen_browser.zen"}, ConfigFile: "zenbrowser.toml"},
 	{ID: "vesktop", Commands: []string{"vesktop"}, Flatpaks: []string{"dev.vencord.Vesktop"}, ConfigFile: "vesktop.toml"},
 	{ID: "equibop", Commands: []string{"equibop"}, ConfigFile: "equibop.toml"},
